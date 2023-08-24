fb9812075e595edb5d39d2a04a0d6a68577b2888
Dilşad <127215115+dilsaddk@users.noreply.github.com>
2023-08-25 02:01:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+function distanceFromInBlocks(pickUpLocation,scuberLocation){
+    return pickUpLocation - scuberLocation;
+    
+
+}
+console.log(distanceFromInBlocks(50,scuberLocation));
+const scuberLocation=42
+
+function distanceTravelledInFeet(start,destination){
+    return (destination - start) * 264
+   
+}
+console.log(distanceTravelledInFeet(34,38));
+
+function calculatesFarePrice(start,destination){
+    const distance=destination-start;
+    
+    if (distance < 400) return 0;
+    if (distance> 400 && distance < 2000) return (destination - start) * 2
+    if (distance > 2000 && distance < 2500) return flatFare
+    if (distance > 2500) return 'cannot travel that far'
+}
+console.log(calculatesFarePrice(800,1000))
